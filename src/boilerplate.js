72447a3e5bad310126c52b7aa7d815279f6d6478
Yash Raj Singh <yashudps.ys@gmail.com>
2024-04-18 21:55:57 +0530

    chore: changing boilerplate

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -1,13 +1,13 @@
 export const BOILERPLATE = {
-  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
-  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
-  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
-  c: `\n#include <stdio.h>\n\nint main() {\n\tprintf("Hello World\\n");\n\treturn 0;\n}\n`,
-  cpp: `\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello World" << std::endl;\n\treturn 0;\n}\n`,
-  go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello World")\n}\n`,
-  kotlin: `\nfun main() {\n\tprintln("Hello World")\n}\n`,
+  javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "from JavaScript!");\n}\n\ngreet("Yash");\n`,
+  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "from TypeScript!");\n}\n\ngreet({ name: "Yash" });\n`,
+  python: `\ndef greet(name):\n\tprint("Hello, " + name + "from Python!")\n\ngreet("Yash")\n`,
+  java: `\npublic class HelloYash {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hello Yash from Java!");\n\t}\n}\n`,
+  c: `\n#include <stdio.h>\n\nint main() {\n\tprintf("Hello Yash from C!\\n");\n\treturn 0;\n}\n`,
+  cpp: `\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello Yash from C++!" << std::endl;\n\treturn 0;\n}\n`,
+  go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello Yash from Go!")\n}\n`,
+  kotlin: `\nfun main() {\n\tprintln("Hello Yash from Kotlin!")\n}\n`,
   csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Alex';\necho $name;\n",
+    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello Yash from C#");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n\n$name = 'Yash';\necho $name;\n",
 };
