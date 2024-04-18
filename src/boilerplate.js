213761eec33b40a3e13fc00bd76b1cce3acd4fc5
Yash Raj Singh <yashudps.ys@gmail.com>
2024-04-18 19:34:56 +0530

    add: support for boilerplate

--- src/boilerplate.js
+++ src/boilerplate.js
@@ -6,6 +6,7 @@ export const BOILERPLATE = {
   c: `\n#include <stdio.h>\n\nint main() {\n\tprintf("Hello World\\n");\n\treturn 0;\n}\n`,
   cpp: `\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello World" << std::endl;\n\treturn 0;\n}\n`,
   go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("Hello World")\n}\n`,
+  kotlin: `\nfun main() {\n\tprintln("Hello World")\n}\n`,
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Alex';\necho $name;\n",
--- src/languages.js
+++ src/languages.js
@@ -1,11 +1,12 @@
 export const LANGUAGES = {
   javascript: "18.15.0",
-  typescript: "5.4.2",
-  python: "3.11.0",
-  java: "22.0.1",
+  typescript: "5.0.3",
+  python: "3.10.0",
+  java: "15.0.2",
   c: "10.2.0",
   cpp: "10.2.0",
   go: "1.16.2",
+  kotlin: "1.8.20",
   csharp: "6.12.0",
   php: "8.2.3",
 };
